0227692c5fc251d7fe09d4958e1293673b2e0a72
Divyansh Chauhan <nishantchauhan44442@gmail.com>
2025-03-31 18:22:52 +0530

    Fixed deployment issues 2

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,12 @@ app.use("/api/message", messageRouter);
 app.use("/api/user", userRouter);
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
   // Set static folder
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
